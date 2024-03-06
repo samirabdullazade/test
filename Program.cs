fd2c820b598da2a0934617c940ae305ee7439d47
samirabdullazade <samirabdullazad4@gmail.com>
2024-03-06 20:27:04 +0400

    added

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+                                            // TASK1
+//ValueType FindCharInex(string soz , char herf)
+//{
+//    for (int i = 0; i < soz.Length; i++) 
+//    {
+//        if (soz[i] == herf) 
+//        {
+//            return herf;
+//        }
+//    }
+//    return -1;
+//};
+
+//Console.WriteLine(FindCharInex("salam", 'a'));
+                                               // TASK3
+//int eded = 9;
+//bool IsPrime(int eded)
+//{
+//    for(int i = 2; i < eded; i++)
+//    {
+//        if (eded % i == 0)  
+//         return false ;
+//    }
+//    return true;
+//}
+//if (eded < 2)
+//{
+//    Console.WriteLine("ne sadedir ne murekkeb");
+//    return;
+//};
+//string result = IsPrime(eded) ? "sadedir" : "murekkebdir";
+//Console.WriteLine(result);
+                                            //TASK4
+//double Muqayise(double eded1 = 4, double eded2 = 6, double eded3 = 1)
+//{
+//    double BoyukEded = eded1;
+//    if(eded2 > eded1)
+//    {
+//            BoyukEded = eded2;
+//    }
+//    if(eded3 > BoyukEded)
+//    {
+//        BoyukEded = eded3;
+//    }
+//    return BoyukEded;
+//}
+//Console.WriteLine(Muqayise);
